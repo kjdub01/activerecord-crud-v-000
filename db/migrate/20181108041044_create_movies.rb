b635e84bdbaebc4e3153a41099425eecc7d3de2b
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-08 04:13:52 +0000

    automatically backed up by learn

--- db/migrate/20181108041044_create_movies.rb
+++ db/migrate/20181108041044_create_movies.rb
@@ -1,5 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     add_columns :movies, :title, :string
+    add_columns :movies, :release_date, :integer
+    add_columns :movies, :director, :string
+    add_columns :movies, :lead, :string
+    add_columns :movies, :in_theaters, :boolean
   end
 end
