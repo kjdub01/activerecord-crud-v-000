181e7cc6dc68f4da8d3b334d95f269bf7c503943
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-08 04:31:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
   __
 end
@@ -20,7 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new(attributes)
 end
 
 def can_be_created_in_a_block(args = __)
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(args = __)
   # release_date == 1990
   
   Movie.create do |m|
-    __
+    
   end
 end
 
